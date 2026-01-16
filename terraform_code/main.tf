c0b3dfd9193de1d2e174a57d063471b680ff95c8
Istvan Uveges <istvan.uveges@nixs.com>
2026-01-16 12:15:36 +0100

    Restructured code

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -48,3 +48,26 @@ resource "proxmox_virtual_environment_vm" "kube_nodes" {
     }
   }
 }
+
+resource "local_file" "ansible_inventory" {
+  filename = "./inventory.ini"
+  content  = <<-EOT
+    [master]
+    %{ for name, vm in proxmox_virtual_environment_vm.kube_nodes ~}
+    %{ if length(regexall("master", name)) > 0 ~}
+    ${name} ansible_host=${split("/", vm.initialization[0].ip_config[0].ipv4[0].address)[0]} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa
+    %{ endif ~}
+    %{ endfor ~}
+
+    [node]
+    %{ for name, vm in proxmox_virtual_environment_vm.kube_nodes ~}
+    %{ if length(regexall("worker", name)) > 0 ~}
+    ${name} ansible_host=${split("/", vm.initialization[0].ip_config[0].ipv4[0].address)[0]} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa
+    %{ endif ~}
+    %{ endfor ~}
+
+    [k3s_cluster:children]
+    master
+    node
+  EOT
+}
